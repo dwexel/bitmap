281939e7e5be840e030edba8649fb7571868eb7e
dex <dexterearley@gmail.com>
2025-03-18 13:27:35 -0500

    more code

--- bmp.c
+++ bmp.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+
 #define DATA_OFFSET_OFFSET 0x000A
 #define WIDTH_OFFSET 0x0012
 #define HEIGHT_OFFSET 0x0016
@@ -15,10 +16,10 @@ typedef unsigned int int32;
 typedef short int16;
 typedef unsigned char byte;
 
-//***Inputs*****
+//****Inputs*****
 //fileName: The name of the file to open 
 //
-//***Outputs****
+//****Outputs****
 //pixels: A pointer to a byte array. This will contain the pixel data
 //width: An int pointer to store the width of the image in pixels
 //height: An int pointer to store the height of the image in pixels
@@ -42,6 +43,9 @@ void ReadImage(const char *fileName, byte **pixels, int32 *width, int32 *height,
         fseek(imageFile, HEIGHT_OFFSET, SEEK_SET);
         fread(height, 4, 1, imageFile);
 
+		printf("reading: %s\n", fileName);
+		printf("width: %u height: %u\n", *width, *height);
+
         //Read bits per pixel
         int16 bitsPerPixel;
         fseek(imageFile, BITS_PER_PIXEL_OFFSET, SEEK_SET);
@@ -50,6 +54,8 @@ void ReadImage(const char *fileName, byte **pixels, int32 *width, int32 *height,
         //Allocate a pixel array
         *bytesPerPixel = ((int32)bitsPerPixel) / 8;
 
+		printf("bytesPerPixel: %u\n", *bytesPerPixel);
+
         //Rows are stored bottom-up
         //Each row is padded to be a multiple of 4 bytes. 
         //We calculate the padded row size in bytes
@@ -57,10 +63,12 @@ void ReadImage(const char *fileName, byte **pixels, int32 *width, int32 *height,
 
         //We are not interested in the padded bytes, so we allocate memory just for
         //the pixel data
-        int unpaddedRowSize = (*width)*(*bytesPerPixel);
+        int unpaddedRowSize = (*width) * (*bytesPerPixel);
+
+		// printf("unpaddedRowSize: %d\n", unpaddedRowSize);
 
         //Total size of the pixel data in bytes
-        int totalSize = unpaddedRowSize*(*height);
+        int totalSize = unpaddedRowSize * (*height);
         *pixels = (byte*)malloc(totalSize);
 
         //Read the pixel data Row by Row.
@@ -166,17 +174,118 @@ void WriteImage(const char *fileName, byte *pixels, int32 width, int32 height,in
         fclose(outputFile);
 }
 
+const char *intenseChars = "-~=a#";
+
+void ReadCell(byte *pixels, int32 unpaddedRowSize, int32 bytesPerPixel, int16 cellWidth, int16 cellHeight, byte cellX, byte cellY)
+{
+
+        // duplicate the pointer to the pixel data
+        byte* pixelPointer = pixels;
+
+        // offset the pointer to the cell location we want.
+        // to get it to the right Y location, offset by the width of a row * number of rows in a cell * number of cells
+        // to get it to the right X location, offset by the width of a cell * number of cells
+        pixelPointer += cellY * unpaddedRowSize * cellHeight;
+        pixelPointer += cellX * (cellWidth * bytesPerPixel);
+
+        byte r;
+        byte intensity;
+
+
+
+        // for each row of pixels in any cell,
+        for (int pd= 0; pd <= cellHeight; pd++) {
+
+                // for each pixel in a row in any cell
+                // cell pixel across
+                for (int cpa = 0; cpa <= cellWidth; cpa++) {
+
+                        // printf("%x,", *(pixelPointer + (cpa * bytesPerPixel)));
+                        // printf("%x,", *(pixelPointer + (j * bytesPerPixel) + 1));
+                        // printf("%x;", *(pixelPointer + (j * bytesPerPixel) + 2));
+                
+                        r = *(pixelPointer + (cpa * bytesPerPixel));
+
+                        // should make a 0-4 integer
+                        intensity = r / 63;
+                        printf("%c", intenseChars[intensity]);
+                        // printf("%u %c ", intensity, intenseChars[intensity]);
+
+
+                        // if (r == 0) {
+                        //         printf("-");
+                        // } else {
+                        //         printf("#");
+                        // }
+
+                }
+
+                printf("\n");
+                pixelPointer += unpaddedRowSize;
+        }
+
+}
+
+// start char?
+void BitmapFont(byte* pixels, int32 width, int32 height, int32 bytesPerPixel, int16 cellWidth, int16 cellHeight) 
+{
+        // repeated code
+        int32 unpaddedRowSize = width * bytesPerPixel;
+
+        int startChar = 32;
+        int whichChar = 1;
+
+        int cellsAcross = width / cellWidth;
+        int cellsDown = height / cellHeight;
+
+	int16 maxCells = 64;
+
+	// for (int i = 0; i < maxCells; i++)
+	// {
+        //         printf("%u,", i % cellsAcross);
+        //         printf("%u; ", i / cellsDown);
+	// 	// printf("%c\n", i);
+        // }
+
+
+
+        int x = whichChar % cellsAcross;
+        int y = whichChar / cellsDown;
+
+        printf("x: %u y: %u\n", x, y);
+        printf("char: %c\n", startChar + whichChar);
+
+        ReadCell(pixels, unpaddedRowSize, bytesPerPixel, cellWidth, cellHeight, x, y);
+
+
+}
+
+// notes:
+// if we're working with a bitmap font image where there's no gap between the last cell in a row and the end of the image
+// then we can do special stuff, right
+// i should figure out a way that incorporates that being a possibility
+
+// terminal colors?
+
+// ouutput hex?
+
+
 int main()
 {
         byte *pixels;
         int32 width;
         int32 height;
         int32 bytesPerPixel;
-        ReadImage("img.bmp", &pixels, &width, &height,&bytesPerPixel);
+
+        // ReadImage("img.bmp", &pixels, &width, &height,&bytesPerPixel);
+        ReadImage("ExportedFont.bmp", &pixels, &width, &height, &bytesPerPixel);
 
 		
 
-        WriteImage("img2.bmp", pixels, width, height, bytesPerPixel);
+        // WriteImage("img2.bmp", pixels, width, height, bytesPerPixel);
+
+	BitmapFont(pixels, width, height, bytesPerPixel, 32, 32);
+
         free(pixels);
         return 0;
 }
